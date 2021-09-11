306fb5fbe7d76efd58c4e34234ae0297ba9bc2c0
weldocarvalho <weldocarvalho@outlook.com>
2021-09-10 21:09:26 -0300

    fix(db_students): col picture add, model add to index

---
+++ src/database/migrations/20210910224959-col-picture-tab-students.js
@@ -0,0 +1,12 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('students', 'picture', {
+      type: Sequelize.STRING,
+      allowNull: true,
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.removeColumn('students', 'picture');
+  },
+};
